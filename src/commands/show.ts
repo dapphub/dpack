60b4bddb34aebfaa3e7b68fcf11fe8a861c75121
Grant Steuart <gmsteuart@gmail.com>
2021-10-21 17:23:43 -0500

    test: show command

--- src/commands/show.ts
+++ src/commands/show.ts
@@ -1,20 +1,21 @@
 import { Command, flags } from '@oclif/command'
-import * as fs from 'fs'
+import * as fs from 'fs-extra'
+
 export default class Show extends Command {
   static description = 'describe the command here'
 
   static flags = {
     help: flags.help({ char: 'h' }),
+    format: flags.help({ char: 'f' })
   }
 
   static args = [{ name: 'file' }]
 
   async run() {
-    const { args, flags } = this.parse(Show)
+    const { args } = this.parse(Show)
 
     if (args.file) {
-      const file = fs.readFileSync(args.file)
-      const json = JSON.parse(file)
+      const json = await fs.readJson(args.file)
       this.log(JSON.stringify(json, null, 2))
     }
   }
--- test/commands/show.test.ts
+++ test/commands/show.test.ts
@@ -1,17 +1,17 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
 
 describe('show', () => {
   test
-  .stdout()
-  .command(['show'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+    .stdout()
+    .command(['show'])
+    .it('runs show', ctx => {
+      expect(ctx.stdout.length).to.equal(0)
+    })
 
   test
-  .stdout()
-  .command(['show', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    .stdout()
+    .command(['show', 'test/sample-pack.json'])
+    .it('runs show test/sample-pack.json', ctx => {
+      expect(ctx.stdout).to.contain('QmS258HYWaBy2zWZpRyUE6xLQZT7KAkTFcMSwn2QHyknWH')
+    })
 })
