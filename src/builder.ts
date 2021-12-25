6afb4f0693fbdc638a820be91b41851f9b087cb2
nikolai <mail@nikolai.fyi>
2021-12-25 09:11:09 -0400

    packType/packObject

--- src/builder.ts
+++ src/builder.ts
@@ -1,18 +1,19 @@
-import { Dpack } from './types'
+import { DPack } from './types'
 import { need, copy } from './util'
-import { putIpfsJson } from './ipfs-util' // TODO replace with sync for `pack`
+import { putIpfsJson } from './ipfs-util'
 import {
   blank,
   merge as _merge,
   addType as _addType,
   addObject as _addObject,
-  assertValidPack
+  assertValidPack,
+  assertValidType
 } from './pure'
 
-const debug = require('debug')('Dpack:builder')
+const debug = require('debug')('DPack:builder')
 
 export class PackBuilder {
-  _pack: Dpack
+  _pack: DPack
   constructor (network: string) {
     need(network, 'new PackBuilder(network) - network must be defined')
     need(typeof (network) === 'string', 'new PackBuilder(network) - network must be a string')
@@ -20,10 +21,31 @@ export class PackBuilder {
     assertValidPack(this._pack)
   }
 
+  async packType (t: any) : Promise<PackBuilder> {
+    const json = JSON.stringify(t.artifact);
+    const cid = (await putIpfsJson(json)).toString();
+    const packed = copy(t);
+    packed.artifact = {'/':cid}
+    this.addType(packed);
+    assertValidPack(this._pack);
+    return Promise.resolve(this);
+  }
+
   addType (t: any): PackBuilder {
+    assertValidType(t);
     this._pack = _addType(this._pack, t)
     assertValidPack(this._pack)
     return this
+  }
+
+  async packObject (o: any) : Promise<PackBuilder> {
+    const json = JSON.stringify(o.artifact);
+    const cid = (await putIpfsJson(json)).toString();
+    const packed = copy(o);
+    packed.artifact = {'/':cid}
+    this.addObject(packed);
+    assertValidPack(this._pack);
+    return Promise.resolve(this);
   }
 
   addObject (o: any): PackBuilder {
@@ -32,7 +54,7 @@ export class PackBuilder {
     return this
   }
 
-  merge (...packs: Dpack[]): PackBuilder {
+  merge (...packs: DPack[]): PackBuilder {
     this._pack = _merge(this._pack, ...packs)
     assertValidPack(this._pack)
     return this
@@ -42,18 +64,6 @@ export class PackBuilder {
     assertValidPack(this._pack)
     const p = copy(this._pack)
     delete p._bundle; delete p._resolved
-    for (const tkey of Object.keys(p.types)) {
-      const t = p.types[tkey]
-      const json = JSON.stringify(t.artifact)
-      const cid = (await putIpfsJson(json)).toString()
-      t.artifact = { '/': cid }
-    }
-    for (const okey of Object.keys(p.objects)) {
-      const o = p.objects[okey]
-      const json = JSON.stringify(o.artifact)
-      const cid = (await putIpfsJson(json)).toString()
-      o.artifact = { '/': cid }
-    }
-    return await Promise.resolve(p)
+    return Promise.resolve(p)
   }
 }
