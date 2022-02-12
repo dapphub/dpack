928a4b8da88180ecd5c07f782bd0d20e57af7839
nikolai <mail@nikolai.fyi>
2022-02-12 11:51:57 -0400

    export aliases

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,13 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
 exports.__esModule = true;
-exports.putIpfsJson = exports.getIpfsJson = exports.Dapp = exports.PackBuilder = void 0;
+exports.putIpfsJson = exports.getIpfsJson = exports.Dapp = exports.PackBuilder = exports.builder = exports.load = void 0;
 var builder_1 = require("./src/builder");
-__createBinding(exports, builder_1, "PackBuilder");
+exports.PackBuilder = builder_1.PackBuilder;
 var dapp_1 = require("./src/dapp");
-__createBinding(exports, dapp_1, "Dapp");
+exports.Dapp = dapp_1.Dapp;
 var ipfs_util_1 = require("./src/ipfs-util");
-__createBinding(exports, ipfs_util_1, "getIpfsJson");
-__createBinding(exports, ipfs_util_1, "putIpfsJson");
+exports.getIpfsJson = ipfs_util_1.getIpfsJson;
+exports.putIpfsJson = ipfs_util_1.putIpfsJson;
+exports.load = dapp_1.Dapp.loadFromJson;
+var builder = function (network) { return new builder_1.PackBuilder(network); };
+exports.builder = builder;
--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-export { PackBuilder } from './src/builder'
-export { Dapp } from './src/dapp'
-export { getIpfsJson, putIpfsJson } from './src/ipfs-util'
+import { PackBuilder } from './src/builder'
+import { Dapp } from './src/dapp'
+import { getIpfsJson, putIpfsJson } from './src/ipfs-util'
+
+export const load = (...args) => {
+//Dapp.loadFromJson
+}
+export const builder = (network) => new PackBuilder(network)
+
+export { PackBuilder, Dapp, getIpfsJson, putIpfsJson }
